a458529b7486829dd083a93a5c67484651ff2c5c
Steven Roland <steven@mojave.io>
2020-04-26 20:09:51 -0400

    Set up tests

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace stvnrlnd\Press\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    /** @test */
+    public function an_initial_test()
+    {
+        $this->assertTrue(true);
+    }
+}
